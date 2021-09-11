fd6ff6b520a97b530837b9987d0181df80b6cbb2
sunil singh <suniljersey201@gmail.com>
2021-09-11 20:16:47 +0530

    It begins

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
+$('.carousel').carousel({
+    pause: 'none',
+    loop:true,
+    margin:10,
+    dots:false,
+    nav:true,
+    mouseDrag:false,
+    autoplay:true,
+    animateOut: 'slideOutUp',
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
